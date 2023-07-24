2618e81cd22e97638de45a7ef5eded925906df56
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-07-24 20:42:23 +0200

    Create entity and repository classes for subscriptions Create SubjectSubscriptionTest class

---
+++ back/src/main/java/com/openclassrooms/mddapi/repository/SubscriptionRepository.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.mddapi.repository;
+
+import com.openclassrooms.mddapi.model.Subscription;
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.model.Subject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
+    Optional<Subscription> findByUserAndSubject(User user, Subject subject);
+}
