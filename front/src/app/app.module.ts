1355e4266c647688c7e73c6ae22620ba05c98577
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-07-25 19:32:30 +0200

    générate subect service, component and model

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SubjectsComponent } from './pages/subjects/subjects/subjects.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, SubjectsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ front/src/app/pages/subjects/subjects/subjects.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subjects-component',
+  templateUrl: './subjects.component.html',
+  styleUrls: ['./subjects.component.scss'],
+})
+export class SubjectsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
