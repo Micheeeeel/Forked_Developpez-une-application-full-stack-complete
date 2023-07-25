d1c5b10d44c3141811e1975c194f7ca28a18dcd3
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-07-25 20:07:24 +0200

    Add Subject service with its unit test onto getSubjects

--- front/src/app/services/subject.service.spec.ts
+++ front/src/app/services/subject.service.spec.ts
@@ -1,16 +1,48 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { SubjectService } from './subject.service';
+import { Subject } from '../models/Subject';
 
 describe('SubjectService', () => {
-  let service: SubjectService;
+  let service: SubjectService; // service we want to test
+  let httpMock: HttpTestingController; // mock of the http service
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SubjectService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // import the HttpClientTestingModule which provides the HttpTestingController
+      providers: [SubjectService], // provide the service we want to test
+    }); // configure the TestBed module
+
+    service = TestBed.inject(SubjectService); // inject the service we want to test
+    httpMock = TestBed.inject(HttpTestingController); // inject the mock of the http service
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // verify that there are no outstanding requests
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve all subjects from the API via GET', () => {
+    const mockSubjects: Subject[] = [
+      { id: 1, name: 'Subject 1' },
+      { id: 2, name: 'Subject 2' },
+    ];
+
+    service.getSubjects().subscribe((subjects: Subject[]) => {
+      expect(subjects.length).toBe(2);
+      expect(subjects).toEqual(mockSubjects);
+    }); // subscribe to the observable returned by the service
+
+    const request = httpMock.expectOne(`${service.baseUrl}/subjects`); // expect that a request will be made to the specified URL via GET
+    expect(request.request.method).toBe('GET'); // expect that the request will be made via GET
+
+    request.flush(mockSubjects); // provide mock data to be returned from the request
   });
 });
--- front/src/app/services/subject.service.ts
+++ front/src/app/services/subject.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Subject } from '../models/Subject';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SubjectService {
-  constructor() {}
+  public baseUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  getSubjects(): Observable<Subject[]> {
+    return this.http.get<Subject[]>(`${this.baseUrl}/subjects`);
+  }
 }
