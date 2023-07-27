8f738738015ecb2a12776605f0b7e565cf06d9a3
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-07-27 13:26:56 +0200

    Add JavaDoc comment and generate doc : Command to execute into the root directory (back) with maven : mvn javadoc:javadoc

--- back/src/main/java/com/openclassrooms/mddapi/controller/SubjectController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/SubjectController.java
@@ -9,6 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.openclassrooms.mddapi.model.Subject;
 import com.openclassrooms.mddapi.repository.SubjectRepository;
 
+// javadoc comments
+/**
+ * The SubjectController class is the controller of the Subject entity.
+ * It allows to get all the subjects from the database.
+ * 
+ * @see Subject
+ * @see SubjectRepository
+ * @see RestController
+ * @see GetMapping
+ * @see Autowired
+ * @see List
+ * @see SubjectRepository#findAll()
+ * @see #getAllSubjects()
+ */
 @RestController
 public class SubjectController {
 
