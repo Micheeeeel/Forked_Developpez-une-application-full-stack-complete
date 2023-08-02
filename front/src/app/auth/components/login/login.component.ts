b724c137613c83304462b224cae6a1644bf24232
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-08-02 18:01:27 +0200

    front: very simple implemention of LoginComponent

--- front/src/app/auth/components/login/login.component.ts
+++ front/src/app/auth/components/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private auth: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onLogin(): void {
+    console.log('Login');
+    this.router.navigateByUrl('/subjects');
   }
-
 }
