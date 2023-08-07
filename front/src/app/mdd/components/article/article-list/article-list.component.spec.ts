8473b56d5a7150131a6a49e36044e805a7e241cb
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-08-07 12:31:54 +0200

    front: configure the test so the resolver should be mocked by loading fake data into the mocked activatedRoute

--- front/src/app/mdd/components/article/article-list/article-list.component.spec.ts
+++ front/src/app/mdd/components/article/article-list/article-list.component.spec.ts
@@ -1,19 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArticleListComponent } from './article-list.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('ArticleListComponent', () => {
   let component: ArticleListComponent;
   let fixture: ComponentFixture<ArticleListComponent>;
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ArticleListComponent ]
-    })
-    .compileComponents();
+      declarations: [ArticleListComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            data: of({
+              articles: [
+                /* vos articles simulés ici */
+              ],
+            }), // Utilisez `of` pour simuler un Observable
+            snapshot: { paramMap: { get: jest.fn() } },
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ArticleListComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArticleListComponent);
+    component = fixture.componentInstance;
+
+    activatedRoute = TestBed.inject(ActivatedRoute);
     fixture.detectChanges();
   });
 
