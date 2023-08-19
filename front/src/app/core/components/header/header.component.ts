193404fa5800e0a75876058bfce071f92203df83
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-08-19 10:11:03 +0200

    front: use an enum HeaderType to handle header appearances as per the context

--- front/src/app/core/components/header/header.component.ts
+++ front/src/app/core/components/header/header.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HeaderType } from 'src/app/app.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Input() headerType!: HeaderType;
+  HeaderTypeEnum = HeaderType; // Ceci créera une référence à l'enum que nous pouvons utiliser dans le template
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
