47d2bdc380d0ef656abe76960f8f064751e8e27c
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-08-19 13:04:55 +0200

    front: create the header navigation element with hamburger menu for small screens

--- front/src/app/shared/material.module.ts
+++ front/src/app/shared/material.module.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   exports: [
@@ -18,6 +19,7 @@ import { MatMenuModule } from '@angular/material/menu';
     MatFormFieldModule,
     MatInputModule,
     MatMenuModule,
+    MatSidenavModule,
   ],
 })
 export class MaterialModule {}
