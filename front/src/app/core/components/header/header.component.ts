47695ab2f9bb3584b2d3be982f12bbf14e24bbc6
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-08-19 13:56:12 +0200

    front: Fix the problem of the sidenav that only opened into the header component, simply by useing the mat-sidenav-container at app.component level.

--- front/src/app/core/components/header/header.component.ts
+++ front/src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { HeaderType } from 'src/app/app.component';
 
 @Component({
@@ -7,10 +7,15 @@ import { HeaderType } from 'src/app/app.component';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() toggleSidenav = new EventEmitter<void>();
   @Input() headerType!: HeaderType;
   HeaderTypeEnum = HeaderType; // Ceci créera une référence à l'enum que nous pouvons utiliser dans le template
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onMenuClick() {
+    this.toggleSidenav.emit();
+  }
 }
