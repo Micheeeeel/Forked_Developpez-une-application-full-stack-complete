f46ed393c462677efd8d9c1aa0d72bd30cdc11c4
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-08-22 11:06:30 +0200

    front: clean up article-list component

--- front/src/app/mdd/components/article/article-list/article-list.component.ts
+++ front/src/app/mdd/components/article/article-list/article-list.component.ts
@@ -1,15 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {
-  Observable,
-  Subject,
-  catchError,
-  map,
-  takeUntil,
-  throwError,
-} from 'rxjs';
+import { Observable, Subject, map } from 'rxjs';
 import { Article } from 'src/app/core/models/Article';
-import { ArticlesService } from 'src/app/mdd/services/articles.service';
 
 @Component({
   selector: 'app-article-list',
@@ -23,54 +15,11 @@ export class ArticleListComponent implements OnInit, OnDestroy {
 
   private unsubscribe$: Subject<boolean> = new Subject<boolean>();
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private service: ArticlesService
-  ) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     this.articles$ = this.route.data.pipe(map((data) => data['articles']));
   }
-
-  handleSuccess(message: string) {
-    console.log(message);
-    this.message = message;
-  }
-
-  handleError(message: string) {
-    console.error(message);
-    this.errorMessage = message;
-  }
-
-  //////
-  onArticleCommented(articleCommented: { comment: string; articleId: number }) {
-    this.service
-      .addNewComment(articleCommented)
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe({
-        next: (message) => {
-          this.handleSuccess('Comment created successfully');
-
-          // Reload the articles
-          this.articles$ = this.reloadArticles();
-        },
-        error: (error) => {
-          this.handleError('Failed to create Comment');
-        },
-      });
-  }
-
-  private reloadArticles(): Observable<Article[]> {
-    return this.service.getArticles().pipe(
-      catchError((error) => {
-        console.error(error);
-        this.errorMessage = 'Error fetching articles';
-        return throwError(() => new Error('Error fetching articles'));
-      })
-    );
-  }
-  //////
 
   CreateArticle() {
     this.router.navigateByUrl('/mdd/article/article-form');
