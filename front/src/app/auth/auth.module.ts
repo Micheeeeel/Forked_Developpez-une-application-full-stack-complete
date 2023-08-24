383b4227018571b59e3e878eccc95e2f58ed0e1f
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-08-24 10:50:06 +0200

    back: create new component for register

--- front/src/app/auth/auth.module.ts
+++ front/src/app/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
 export class AuthModule {}
